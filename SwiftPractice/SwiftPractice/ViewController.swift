97f735d9aa15048c19b3bd6577d9c13e60d2b494
Raj <raj.kp9@achiever-87.local>
2018-01-11 17:37:40 +0530

    Commit--1

---
+++ SwiftPractice/SwiftPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPractice
+//
+//  Created by Raj Kumar on 1/11/18.
+//  Copyright © 2018 Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
